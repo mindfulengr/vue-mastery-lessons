cc0d7101b9ee71278083ead1394abb1d5d1cccab
Paul Werner <mindfulengr@gmail.com>
2019-12-27 16:18:16 +0100

    add app bar and footer

--- beautify-with-vuetify/vuetify-dashboard/src/App.vue
+++ beautify-with-vuetify/vuetify-dashboard/src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <v-app>
+    <v-app-bar app color='primary' dark>
+      <v-toolbar-title>
+        Vuetify Dashboard
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
+    </v-app-bar>
+    <v-content>
+
+    <!-- Login Module -->
    <v-card width="400" class="mx-auto mt-5">
      <v-card-title>
        <h1 class="display-1">Login</h1>
@@ -23,6 +34,36 @@
        <v-btn color="info">Login</v-btn>
      </v-card-actions>
     </v-card>
+    </v-content>
+  
+    <v-footer
+      color="primary lighten-1"
+      padless
+    >
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col
+          class="primary lighten-2 py-4 text-center white--text"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  
+  
   </v-app>
 </template>
 
@@ -35,6 +76,10 @@ export default {
   },
 
   data: () => ({
+    links: [
+        'Home',
+        'Login'
+      ],
     showPassword: false
   }),
 };
