979fb445e00dcd62961301af91ead52d344c19cd
Paul Werner <mindfulengr@gmail.com>
2019-12-27 17:25:48 +0100

    use dashboard components

--- beautify-with-vuetify/vuetify-dashboard/src/views/Dashboard.vue
+++ beautify-with-vuetify/vuetify-dashboard/src/views/Dashboard.vue
@@ -1,105 +1,64 @@
 <template>
   <div>
     <h1>Dashboard</h1>
-    <v-data-table
-      :headers="headers"
-      :items="employees"
-      :items-per-page="5"
-      class="elevation-1"
-      @click:row="selectRow"
-      multi-sort
-    ></v-data-table>
+
+    <SalesGraph v-for="sale in sales" :key="`${sale.title}`" :sale="sale" />
+
+    <StatisticCard
+      v-for="statistic in statistics"
+      :key="`${statistic.title}`"
+      :statistic="statistic"
+    />
+
+    <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+
+    <EventTimeline :timeline="timeline" />
+
     <v-snackbar v-model="snackbar">
-      You have selected {{ currentItem.name }} the {{ currentItem.title }}
+      You have selected {{ selectedEmployee.name }},
+      {{ selectedEmployee.title }}
       <v-btn color="pink" text @click="snackbar = false">Close</v-btn>
     </v-snackbar>
   </div>
 </template>
+
 <script>
+import EmployeesTable from "../components/EmployeesTable";
+import EventTimeline from "../components/EventTimeline";
+import SalesGraph from "../components/SalesGraph";
+import StatisticCard from "../components/StatisticCard";
+
+import employeesData from "../data/employees.json";
+import timelineData from "../data/timeline.json";
+import salesData from "../data/sales.json";
+import statisticsData from "../data/statistics.json";
+
 export default {
+  name: "DashboardPage",
+  components: {
+    EmployeesTable,
+    EventTimeline,
+    SalesGraph,
+    StatisticCard
+  },
   data() {
     return {
-      currentItem: {},
+      employees: employeesData,
+      sales: salesData,
+      selectedEmployee: {
+        name: "",
+        title: ""
+      },
       snackbar: false,
-      headers: [
-        {
-          text: "Employee ID",
-          align: "left",
-          sortable: false,
-          value: "id"
-        },
-        { text: "Name", value: "name" },
-        { text: "Title", value: "title" },
-        { text: "Salary", value: "salary" }
-      ],
-      employees: [
-        {
-          id: 12875,
-          name: "Dwight Schrute",
-          title: "Assistant to the Regional Manager",
-          salary: 65000
-        },
-        {
-          id: 14094,
-          name: "Kelly Kapoor",
-          title: "Customer Service Representative",
-          salary: 40000
-        },
-        {
-          id: 12894,
-          name: "Darryl Philbin",
-          title: "Warehouse Foreman",
-          salary: 68000
-        },
-        {
-          id: 13893,
-          name: "Angela Martin",
-          title: "Accountant",
-          salary: 54000
-        },
-        {
-          id: 12943,
-          name: "Phyllis Vance",
-          title: "Saleswoman",
-          salary: 65000
-        },
-        {
-          id: 19523,
-          name: "Karen Filippeli",
-          title: "Saleswoman",
-          salary: 65000
-        },
-        {
-          id: 18349,
-          name: "Toby Flenderson",
-          title: "Human Resources Representative",
-          salary: 60000
-        },
-        {
-          id: 11853,
-          name: "Michael Scott",
-          title: "Regional Manager",
-          salary: 70000
-        },
-        {
-          id: 14821,
-          name: "Pam Beasley",
-          title: "Secretary",
-          salary: 45000
-        },
-        {
-          id: 15834,
-          name: "Jim Halpert",
-          title: "Salesman",
-          salary: 65000
-        }
-      ]
+      statistics: statisticsData,
+      timeline: timelineData
     };
   },
   methods: {
-    selectRow(event) {
+    setEmployee(event) {
       this.snackbar = true;
-      this.currentItem = event;
+      this.selectedEmployee.name = event.name;
+      this.selectedEmployee.title = event.title;
     }
   }
 };
