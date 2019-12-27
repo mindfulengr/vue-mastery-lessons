4464b25ed06b456c4db21ac5250c936caafa50cf
Paul Werner <mindfulengr@gmail.com>
2019-12-27 16:40:27 +0100

    use login component and add button links

--- beautify-with-vuetify/vuetify-dashboard/src/App.vue
+++ beautify-with-vuetify/vuetify-dashboard/src/App.vue
@@ -5,35 +5,20 @@
         Vuetify Dashboard
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        text
+        rounded
+        :to='link.url'
+      >
+        {{ link.label }} 
+      </v-btn>
     </v-app-bar>
-    <v-content>
 
-    <!-- Login Module -->
-   <v-card width="400" class="mx-auto mt-5">
-     <v-card-title>
-       <h1 class="display-1">Login</h1>
-     </v-card-title>
-     <v-card-text>
-       <v-form>
-        <v-text-field 
-        prepend-icon="mdi-account-circle" 
-        label="Username"/>
-        <v-text-field
-        :type="showPassword ? 'text' : 'password'" 
-        prepend-icon="mdi-lock" 
-        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" 
-        label="Password"
-        @click:append="showPassword = !showPassword"/>
-       </v-form>
-     </v-card-text>
-     <v-card-actions>
-       <v-btn color="success">Register</v-btn>
-       <v-spacer></v-spacer>
-       <v-btn color="info">Login</v-btn>
-     </v-card-actions>
-    </v-card>
+    <v-content>
+      <router-view></router-view>
     </v-content>
   
     <v-footer
@@ -46,13 +31,14 @@
       >
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer-link`"
           color="white"
           text
           rounded
           class="my-2"
+          :to="link.url"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-col
           class="primary lighten-2 py-4 text-center white--text"
@@ -77,9 +63,14 @@ export default {
 
   data: () => ({
     links: [
-        'Home',
-        'Login'
-      ],
+      {
+        label: 'Home',
+        url: '/'
+      },{
+        label: 'Login',
+        url: '/login'
+      }
+    ],
     showPassword: false
   }),
 };
