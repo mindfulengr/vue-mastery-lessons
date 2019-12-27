9280f1790f37642054d7905ff1c099d0b729fe21
Paul Werner <mindfulengr@gmail.com>
2019-12-27 19:11:26 +0100

    add validation rules

--- beautify-with-vuetify/vuetify-dashboard/src/views/Signup.vue
+++ beautify-with-vuetify/vuetify-dashboard/src/views/Signup.vue
@@ -4,7 +4,7 @@
       <v-col>
         <h1>Singup</h1>
         <v-form>
-          <v-text-field type="text" label="Email"></v-text-field>
+          <v-text-field type="text" label="Email" :rules="emailRules"></v-text-field>
           <v-autocomplete :items="browsers" label="Which browser do you use?"></v-autocomplete>
           <v-file-input label="Atach profile photo"></v-file-input>
           <v-menu
@@ -20,7 +20,11 @@
             </template>
             <v-date-picker v-model="birthday" @input="menu = false"></v-date-picker>
           </v-menu>
-          <v-checkbox label="Agree to terms & conditions"></v-checkbox>
+          <v-checkbox
+            label="Agree to terms & conditions"
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+          ></v-checkbox>
           <v-btn type="submit" color="primary">Submit</v-btn>
         </v-form>
       </v-col>
@@ -32,6 +36,22 @@
 export default {
   data: () => ({
     menu: false,
+    agreeToTerms: false,
+    agreeToTermsRules: [
+      v =>
+        !!v ||
+        "You must agree to the terms and conditions to sign up for an account"
+    ],
+    emailRules: [
+      v => !!v || "Email is required.",
+      v => (v && v.includes("@")) || "Email should have an @ symbol",
+      v =>
+        (v && v.indexOf(".") - v.indexOf("@") > 1) ||
+        "Email should contain a valid domain",
+      v =>
+        (v && v.indexOf(".") <= v.length - 3) ||
+        "Email should have a valid domain extension"
+    ],
     birthday: "",
     browsers: ["Chrome", "Firefox", "Safari", "Edge", "Brave"]
   })
