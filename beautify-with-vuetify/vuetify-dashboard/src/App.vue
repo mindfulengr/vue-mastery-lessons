e0ebc2c08d643c6b82b563e08c0f32abe58a2620
Paul Werner <mindfulengr@gmail.com>
2019-12-27 18:25:59 +0100

    add signup route

--- beautify-with-vuetify/vuetify-dashboard/src/App.vue
+++ beautify-with-vuetify/vuetify-dashboard/src/App.vue
@@ -1,34 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar app color='primary' dark>
-      <v-toolbar-title>
-        Vuetify Dashboard
-      </v-toolbar-title>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      
+
       <v-btn
         v-for="link in links"
         :key="`${link.label}-header-link`"
         text
         rounded
-        :to='link.url'
-      >
-        {{ link.label }} 
-      </v-btn>
+        :to="link.url"
+      >{{ link.label }}</v-btn>
     </v-app-bar>
 
     <v-content>
       <router-view></router-view>
     </v-content>
-  
-    <v-footer
-      color="primary lighten-1"
-      padless
-    >
-      <v-row
-        justify="center"
-        no-gutters
-      >
+
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
         <v-btn
           v-for="link in links"
           :key="`${link.label}-footer-link`"
@@ -37,46 +27,42 @@
           rounded
           class="my-2"
           :to="link.url"
-        >
-          {{ link.label }}
-        </v-btn>
-        <v-col
-          class="primary lighten-2 py-4 text-center white--text"
-          cols="12"
-        >
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        >{{ link.label }}</v-btn>
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} —
+          <strong>Vuetify</strong>
         </v-col>
       </v-row>
     </v-footer>
-  
-  
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
-
-  },
+  components: {},
 
   data: () => ({
     links: [
       {
-        label: 'Home',
-        url: '/'
+        label: "Home",
+        url: "/"
       },
       {
-        label: 'Login',
-        url: '/login'
+        label: "Login",
+        url: "/login"
       },
       {
-        label: 'Dashboard',
-        url: '/dashboard'
+        label: "Signup",
+        url: "/signup"
+      },
+      {
+        label: "Dashboard",
+        url: "/dashboard"
       }
     ],
     showPassword: false
-  }),
+  })
 };
 </script>
